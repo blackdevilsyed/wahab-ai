413442ff4cd43a11e5adec6c95c0d4c087106203
blackdevilsyed <syedabdulwahabbukhari8@gmail.com>
2026-05-08 19:19:33 +0500

    Create chat.js

---
+++ api/chat.js
@@ -0,0 +1,57 @@
+export default async function handler(req, res) {
+
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      error: "Method not allowed"
+    });
+  }
+
+  try {
+
+    const { message } = req.body;
+
+    const response = await fetch(
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyC5U9FE6CmUACIXH7K46qLI20WlPX2zauQ",
+      {
+        method: "POST",
+
+        headers: {
+          "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({
+
+          contents: [
+            {
+              parts: [
+                {
+                  text: message
+                }
+              ]
+            }
+          ]
+
+        })
+
+      }
+    );
+
+    const data = await response.json();
+
+    const ai =
+      data.candidates[0]
+      .content.parts[0].text;
+
+    res.status(200).json({
+      reply: ai
+    });
+
+  } catch (err) {
+
+    res.status(500).json({
+      reply: "AI Error"
+    });
+
+  }
+
+            }
