6c37295833b6d43db146c59506fbc2129dc019bd
blackdevilsyed <syedabdulwahabbukhari8@gmail.com>
2026-05-08 19:35:46 +0500

    Change export syntax from default to module.exports

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
 
   if (req.method !== "POST") {
     return res.status(405).json({
@@ -54,4 +54,4 @@ export default async function handler(req, res) {
 
   }
 
-            }
+};
